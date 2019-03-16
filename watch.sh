f902397a437de2f55f3cb76c1322c63b10b1b059
Gerd Aschemann <gerd@aschemann.net>
2019-03-16 15:10:15 +0100

    Watch JS app changes to re-build + re-deploy

--- watch.sh
+++ watch.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 # watch the java files and continously deploy the service
-mvn clean install
+mvn clean package -Platest,docker-build
 skaffold run -p dev
-reflex -r "\.java$" -- bash -c 'mvn install && skaffold run -p dev'
+reflex -v \
+    -g '*.js' \
+    -g '*.css' \
+    -g '*.html' \
+    -g '*.json' \
+    -g '*.gif' \
+    -g '*.png' \
+    -g '*.jpg' \
+    -- bash -c 'mvn package -Platest,docker-build && skaffold run -p dev'
